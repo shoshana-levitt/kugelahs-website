4d6211c961ce1c4a4786258282c9b68117dae574
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-19 11:14:51 -0800

    change text to black, change navlinks

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,7 +16,7 @@ const Navbar = () => {
           <Link
             href={link.href}
             key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            className="regular-16 text-black flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
           >
             {link.label}
           </Link>
--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,7 @@
 export const NAV_LINKS = [
   { href: "/", key: "home", label: "Home" },
   { href: "/about-me", key: "about_me", label: "About Me" },
+  { href: "/media", key: "media", label: "Media" },
   { href: "/kugels", key: "my_kugels", label: "My Kugels" },
-  { href: "/events", key: "events ", label: "Events" },
+  { href: "/buy", key: "buy ", label: "Buy" },
 ];
