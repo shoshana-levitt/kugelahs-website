4168c09e7f8b97a935ca39f7c83f61914d0c50af
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-19 12:41:57 -0800

    edit AboutMe.tsx text

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -3,26 +3,26 @@ import React from "react";
 const AboutMe = () => {
   return (
     <div>
-      <h1>How It All Started</h1>
-      <p>
+      <h1 className="py-3">How It All Started</h1>
+      <p className="py-3">
         While I was finding myself in NYC, I took a break in 2021 and went home
-        for the Jewish high holidays. I made the same kugel I make every year,
+        for the Jewish High Holy Days. I made the same kugel I make every year,
         except this time it was without the help of my mom. My brother, with
         much enthusiasm, said “This kugel is delicious, Robin, you need to sell
         this!” So, I am doing just that :)
       </p>
-      <h1>Behind The Name</h1>
-      <p>
-        The name ‘Kugelahs’ came upon me all of a sudden, literally in my
-        dreams, and I was set on that name ever since. it reflects my upbringing
+      <h1 className="py-3">Behind The Name</h1>
+      <p className="py-3">
+        The name "Kugelahs" came upon me all of a sudden, literally in my
+        dreams, and I was set on that name ever since. It reflects my upbringing
         and what I want to convey through my kugels. Growing up in an Ashkenazi
-        jewish family, we always used the term of endearment, ‘Bubelah’. It’s a
-        yiddish word that translates to sweetie. It creates a sense of nostalgia
-        and warmth. I always knew when things were tough my family called me
-        bubelah and always ensured me that everything would be ok. Kugelahs was
-        inspired by this feeling and word. My hope is that when someone sees the
-        name and eats my kugel they are immediately overcome with a sense of
-        safety and feel at home.
+        Jewish family, we always used the term of endearment, "bubelah." It’s a
+        Yiddish word that translates to "sweetie." It creates a sense of
+        nostalgia and warmth. I always knew when things were tough, my family
+        called me bubelah and always ensured me that everything would be ok.
+        Kugelahs was inspired by this feeling and word. My hope is that when
+        someone sees the name and eats my kugel, they are immediately overcome
+        with a sense of safety and feel at home.
       </p>
     </div>
   );
