798550f6fe7f7f77151ec82efc235c157b3c1685
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-19 12:37:53 -0800

    add pagecontainer class, apply to about-me index.js, add text to AboutMe.tsx

--- pages/about-me/index.js
+++ pages/about-me/index.js
@@ -3,11 +3,15 @@ import AboutMe from "@/components/AboutMe";
 
 export default function AboutMePage() {
   return (
-    <div>
+    <div className="pagecontainer">
       <Head>
         <title>Kugelas - About Me</title>
       </Head>
-      <AboutMe />
+      <div>
+        <AboutMe />
+      </div>
     </div>
   );
 }
+
+// pt-60 ml-[50px]
