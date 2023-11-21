13661cd12c18f01fc4e21e5d218994825b2918c6
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2023-11-21 09:50:37 -0800

    chnage Navbar to MUI, change colors

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,35 +1,41 @@
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import { Button, AppBar, Toolbar, IconButton, Box, Stack } from "@mui/material";
 
 const Navbar = () => {
   return (
-    <nav className="flexBetween max-container padding-container fixed z-30 py-5">
-      <ul className="hidden h-full gap-12 lg:flex">
-        <li>
-          {" "}
-          <Link href="/home">
-            <Image src="/logo.jpeg" alt="logo" width={150} height={150} />
-          </Link>
-        </li>
-        {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 text-black flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
-            {link.label}
-          </Link>
-        ))}
-      </ul>
-      <Image
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer lg:hidden"
-      />
-    </nav>
+    <div>
+      <AppBar
+        position="fixed"
+        style={{ backgroundColor: "rgb(248, 182, 226)", boxShadow: "none" }}
+      >
+        <Toolbar sx={{ p: 1 }}>
+          <Box sx={{ display: "flexBetween", flexGrow: 1 }}>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={5}
+            >
+              <Link href="/home">
+                <Image src="/logo.jpeg" alt="logo" width={75} height={75} />
+              </Link>
+              {NAV_LINKS.map((link) => (
+                <Link
+                  href={link.href}
+                  key={link.key}
+                  className="regular-16 text-black flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+                >
+                  {link.label}
+                </Link>
+              ))}
+            </Stack>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar></Toolbar>
+    </div>
   );
 };
 
