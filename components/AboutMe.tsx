93d2e64ff045478bc4c89390e5d6c9f511be7818
shoshana.levitt@gmail.com <shoshana.levitt@gmail.com>
2024-01-08 17:15:18 -0500

    change text to Typography on About Me page

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 import { motion, useAnimation } from "framer-motion";
+import { Typography } from "@mui/material";
 
 const AboutMe = () => {
   const pageVariant = {
@@ -29,16 +30,21 @@ const AboutMe = () => {
         initial="hidden"
         animate={control}
       >
-        <h1 className="py-3">How It All Started</h1>
-        <p className="py-3">
+        <Typography variant="h3" padding={2}>
+          How It All Started
+        </Typography>
+        <Typography variant="h6" padding={2}>
+          {" "}
           While I was finding myself in NYC, I took a break in 2021 and went
           home for the Jewish High Holy Days. I made the same kugel I make every
           year, except this time it was without the help of my mom. My brother,
           with much enthusiasm, said “This kugel is delicious, Robin, you need
-          to sell this!” So, I am doing just that :)
-        </p>
-        <h1 className="py-3">Behind The Name</h1>
-        <p className="py-3">
+          to sell this!” So, I am doing just that ☺
+        </Typography>
+        <Typography variant="h3" padding={2}>
+          Behind The Name
+        </Typography>
+        <Typography variant="h6" padding={2}>
           The name "Kugelahs" came upon me all of a sudden, literally in my
           dreams, and I was set on that name ever since. It reflects my
           upbringing and what I want to convey through my kugels. Growing up in
@@ -49,7 +55,7 @@ const AboutMe = () => {
           everything would be ok. Kugelahs was inspired by this feeling and
           word. My hope is that when someone sees the name and eats my kugel,
           they are immediately overcome with a sense of safety and feel at home.
-        </p>
+        </Typography>
       </motion.div>
     </div>
   );
